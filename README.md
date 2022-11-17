8a6fd9bd458ab751026d55b066a69014b55c08d6
MatiasDonati <matod87@hotmail.com>
2022-11-17 18:00:40 -0300

    identacion

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Yanilog eCommerce
 
 Tienda de Arte Digital, Ilustraciones y Pinturas.
+
+https://tailwindcss.com/docs/installation
+
+https://daisyui.com/components/toggle/
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,38 +4,37 @@ const NavBar = () => {
   return (
     <div className="navbar bg-base-100">
         <div className="flex-1">
-
+            <a className="btn btn-ghost normal-case text-xl">Yanilog</a>
             <a className="btn btn-ghost normal-case text-xl">Ilustraciones</a>
             <a className="btn btn-ghost normal-case text-xl">Arte Digital</a>
             <a className="btn btn-ghost normal-case text-xl">Pinturas</a>
         </div>
-        <div className="flex-1">
-            <a className="btn btn-ghost normal-case text-xl">Yanilog</a>
-        </div>
+
         <div className="form-control">
             <input type="text" placeholder="Buscar" className="input input-bordered" />
         </div>
+
         <div className="flex-none">
             <div className="dropdown dropdown-end">
             {/* Componente CartWidget dentro de NavBar */}
             <CartWidget />
             </div>
+
             <div className="dropdown dropdown-end">
-            <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+                <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
                 <div className="w-10 rounded-full">
                 <img src="https://placeimg.com/80/80/people" />
                 </div>
-            </label>
-            <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                <li>
-                <a className="justify-between">
-                    Perfil
-                    <span className="badge">Nuevo</span>
-                </a>
-                </li>
-                <li><a>Ajustes</a></li>
-                <li><a>YaniLogout</a></li>
-            </ul>
+                </label>
+                <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                    <li>
+                        <a className="justify-between">Perfil
+                        <span className="badge">Nuevo</span>
+                        </a>
+                    </li>
+                    <li><a>Ajustes</a></li>
+                    <li><a>YaniLogout</a></li>
+                </ul>
             </div>
         </div>
     </div>
