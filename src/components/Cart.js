24e5e09308b87d50357f3b52a9e87658e8d7b815
MatiasDonati <matod87@hotmail.com>
2022-12-18 09:16:03 -0300

    cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,19 +1,30 @@
 
+import { Link } from "react-router-dom";
 import { useCart } from "../context/CartContext"
 import ProductoEnCart from "./ProductoEnCart"
 
 const Cart = () => {
 
-  const { items, carroVacio , clearCart } = useCart()
+  const { items, carroVacio, clearCart } = useCart()
   console.log(carroVacio);
 
-  console.log(items);
+  let totalPrice = items.reduce((acc, item) => acc + item.price, 0);
+
+  console.log(items.length);
   return (
     <div className="m-10 text-xl">
-      <strong>Cart</strong>
-      {carroVacio ? <p>No tienes Obras en el Carrito</p> : <></>}
-      <p>{ items.map( product =>  <ProductoEnCart key={product.id} {...product}/>) }</p>
+      <strong>Carrito de Compras</strong>
+      <br />
+      {totalPrice > 0 && <strong>Precio total: $ {totalPrice}</strong>}
+      <p>{items.map(product => <ProductoEnCart key={product.id} {...product} />)}</p>
+      {carroVacio==true ?
+      <>
+      <p>El carrito se encuentra vacío</p>
+      <Link to='/'><button className="btn">Ir a comprar</button></Link>
+      </>
+      :
       <button className="btn" onClick={clearCart}>Vaciar Carrito</button>
+      }
     </div>
   )
 }
